8a25183c8c4eb9d6bd72257620ba9c711bafb2c5
gauthier <gauthier.sayarath@gmail.com>
2020-06-14 15:18:34 +0200

    Fix controller and request tests for contact routes

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -4,19 +4,20 @@ module Api
   module V1
     class ContactsController < ApplicationController
       before_action :set_contact, only: [:update, :destroy]
-      before_action :check_user, except: [:index]
+      before_action :set_job_application, only: [:index, :create]
+      before_action :check_user
 
       def index
-        @contacts = current_user.contacts
+        @contacts = @job_application.contacts
 
         render json: @contacts
       end
 
       def create
-        @contact = Contact.new(contact_params)
+        @contact = Contact.new(contact_params.merge(job_application: @job_application))
 
         if @contact.save
-          render json: @contact, status: :created, location: api_v1_contacts_url(@contact)
+          render json: @contact, status: :created, location: api_v1_job_application_contacts_path(@contact)
         else
           render json: @contact.errors, status: :unprocessable_entity
         end
@@ -40,12 +41,17 @@ module Api
         @contact = Contact.find(params[:id])
       end
 
+      def set_job_application
+        @job_application = JobApplication.find(params[:job_application_id])
+      end
+
       def contact_params
-        params.require(:contact).permit(:type, :description, :date, :is_done).merge(job_application: JobApplication.find(params[:job_application]))
+        params.require(:contact).permit(:email, :first_name, :last_name, :phone_number, :position)
       end
 
       def check_user
-        render json: { error: 'Unauthorized' } if current_user != @contact.job_application.user
+        owner = @contact ? @contact.job_application.user : @job_application.user
+        render json: { error: 'Unauthorized' }, status: :unauthorized if current_user != owner
       end
     end
   end
