e5178a070e46f3ed423fb48e78db0f90f74a640c
gauthier <gauthier.sayarath@gmail.com>
2020-06-14 15:17:38 +0200

    Request tests for profile routes

--- spec/requests/api/v1/profile_request_spec.rb
+++ spec/requests/api/v1/profile_request_spec.rb
@@ -3,4 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Profiles", type: :request do
+  before do
+    @user = create(:user)
+    sign_in(@user)
+  end
+
+  describe 'GET /api/v1/profile' do
+    before do
+      get "/api/v1/profile"
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'returns the right user' do
+      expect(JSON.parse(response.body)['id']).to eq(@user.id)
+    end
+  end
 end
