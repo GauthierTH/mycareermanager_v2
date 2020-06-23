419bbe45660a87a4153f2e8d3800c639afc7176e
gauthier <gauthier.sayarath@gmail.com>
2020-06-23 17:57:14 +0200

    Test config react router on prod

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,6 +24,10 @@ class ApplicationController < ActionController::API
     }, status: :bad_request
   end
 
+  def fallback_index_html
+    render file: 'public/index.html'
+  end
+
   protected
 
   def configure_permitted_parameters
