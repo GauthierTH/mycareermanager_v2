ef3e0a235a8c3f4c6db780521a076c24f814c756
gauthier <gauthier.sayarath@gmail.com>
2020-07-03 16:35:01 +0200

    Use formatCategoryHelper in NextStepCard(home_page)

--- client/src/pages/home/NextStepCard.js
+++ client/src/pages/home/NextStepCard.js
@@ -3,12 +3,13 @@ import moment from 'moment'
 import { Link } from "react-router-dom";
 
 import { shortenStringHelper } from 'helpers/shortenString'
+import { formatStepCategoryHelper } from 'helpers/formatStepCategory'
   
 const NextStepCard = ({category, date, company_name, position, jobApplicationId}) => {
   return(
     <div className='card'>
       <div className='justify-content-between align-items-center'>
-        <div className='badge orange-bright-background'>{category}</div>
+        <div className='badge orange-bright-background'>{formatStepCategoryHelper[category]}</div>
         <div><Link to={`/job-application/${jobApplicationId}`}><h3>{position}</h3></Link></div>
       </div>
       <div className='justify-content-between align-items-center'>
