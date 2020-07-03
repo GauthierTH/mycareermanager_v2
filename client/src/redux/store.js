f706e2a217190c09a559ab075d6628dbec2e9c8a
GauthierTH <45210759+GauthierTH@users.noreply.github.com>
2020-07-03 14:50:57 +0200

    Revert "Fix redux compose on prod (#78)"
    This reverts commit 21dbc7194d75e10329e7db9e32faf0b98d6cbde0.

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -10,7 +10,7 @@ const rootReducer = combineReducers({
 
 const store = createStore(
   rootReducer,
-  compose(applyMiddleware(thunkMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f)
+  composeWithDevTools(applyMiddleware(thunkMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 )
 
 export default store
