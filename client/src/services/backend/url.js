50c5ca724b78dee3ff20ace3e8e4164104a58331
gauthier <gauthier.sayarath@gmail.com>
2020-07-05 15:36:50 +0200

    Remove force staging url for prod

--- client/src/services/backend/url.js
+++ client/src/services/backend/url.js
@@ -1,2 +1,2 @@
-export const URL = process.env.NODE_ENV !== 'production' ? 'http://localhost:3001' : '/'
+export const URL = process.env.NODE_ENV !== 'production' ? 'http://localhost:3001' : ''
 export const API_URL = process.env.NODE_ENV !== 'production' ? 'http://localhost:3001/api/v1' : '/api/v1'
