25c2bb6d32374ba152849d4f3191e98aa082146a
gauthier <gauthier.sayarath@gmail.com>
2020-07-05 15:26:44 +0200

    Remove force staging url for prod

--- client/src/services/backend/url.js
+++ client/src/services/backend/url.js
@@ -1,2 +1,2 @@
-export const URL = process.env.NODE_ENV !== 'production' ? 'http://localhost:3001' : 'https://mycareermanager-staging.herokuapp.com/'
-export const API_URL = process.env.NODE_ENV !== 'production' ? 'http://localhost:3001/api/v1' : 'https://mycareermanager-staging.herokuapp.com/api/v1'
+export const URL = process.env.NODE_ENV !== 'production' ? 'http://localhost:3001' : '/'
+export const API_URL = process.env.NODE_ENV !== 'production' ? 'http://localhost:3001/api/v1' : '/api/v1'
